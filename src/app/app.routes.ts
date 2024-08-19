d515161f5a00abf1021a8e457dc85b854a620959
Olepir Andrew <olepir.andrew@gmail.com>
2024-08-19 18:15:15 +0300

    performers-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PerformersListComponent } from './performers-list/performers-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PerformersListComponent},
+];
